3d037d3e3c1a22cde70d070a34d739c9a6a8f4f2
Mikhail Kelebeev <m@kelebeev.ru>
2023-07-20 19:30:13 +0400

    update

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from ice_cream.models import IceCream
+from django.db.models import Q
 
 def index(request):
+    ice_cream_list = IceCream.objects.values('id', 'title', 'description', c'category__title')
     template = 'homepage/index.html'
-    return render(request, template)
+    # Запишите в переменную ice_cream_list новый QuerySet
+    context = {
+        'ice_cream_list': ice_cream_list,
+    }
+    return render(request, template, context)
